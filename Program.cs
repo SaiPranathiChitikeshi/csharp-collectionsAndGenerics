a5b08bea93e37c819a604f8f487e1729df1e0070
SaiPranathiChitikeshi <75035288+SaiPranathiChitikeshi@users.noreply.github.com>
2020-12-04 16:22:06 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace arr
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            int[] ar=new int[] {1,2,3,5,4};
+            int b=6;
+            var r=sumtarget(ar,b);
+            Console.WriteLine(r);
+
+        }
+        public static bool sumtarget(int[] arr,int target){
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int k = 0; k < arr.Length; k++)
+                {
+                    if (i != k)
+                    {
+                        int sum = arr[i] + arr[k];
+                        if (sum == target)
+                            return true;
+                    }
+                }
+            }
+            return false;
+        }
+    }
+}
