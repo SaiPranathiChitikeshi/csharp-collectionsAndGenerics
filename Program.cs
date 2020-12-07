1909e0563d2abee57830e72752ac74c894c3722a
SaiPranathiChitikeshi <75035288+SaiPranathiChitikeshi@users.noreply.github.com>
2020-12-07 12:01:09 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+namespace genlis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum=0;
+            List<int> n=new List<int>();
+            n.Add(20);
+            n.Add(3);
+            n.Add(13);
+            n.Add(17);
+            n.Add(50);
+            foreach (int t in n)
+            {
+                sum+=t;
+            }
+            System.Console.WriteLine(sum);
+            n.Remove(13);
+            Console.WriteLine(n.BinarySearch(50));
+        }
+    }
+}
